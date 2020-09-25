468754d219d7b77da3ace395dfbc5047a0312a67
Ramanan663 <69298210+Ramanan663@users.noreply.github.com>
2020-09-25 17:07:53 +0200

    Add files via upload

---
+++ jscript2/script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy studenten");
+//"Dit is een grote som"
+/*let x = [1230941];
+let y = 1823794;
+console.log(x * y);
+let num1 = 637263;
+let num2 = 54;
+console.log(num1 / num2);*/
