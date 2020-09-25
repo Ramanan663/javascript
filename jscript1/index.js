3851bbbe3449af05d7cf8af072e2b36d9625eb64
Ramanan663 <69298210+Ramanan663@users.noreply.github.com>
2020-09-25 11:25:20 +0200

    Add files via upload

---
+++ jscript1/index.js
@@ -0,0 +1,12 @@
+let name = "Ramanan";
+console.log(name);
+//twee string atw=44
+let number = 4;
+console.log(number + number);
+console.log(number - number);
+console.log(number * number);
+console.log(number / number);
+console.log(number % number);
+//10.
+let age = 48;
+console.log(age);
