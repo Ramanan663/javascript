b0fbeb7960c42b291c1403b92d4c27d4aacef69d
Ramanan663 <69298210+Ramanan663@users.noreply.github.com>
2020-09-28 15:03:24 +0200

    Add files via upload

---
+++ jscript3/script.js
@@ -0,0 +1,18 @@
+const age = 20;
+const isFemale = true;
+const driverStatus = "bob";
+if (age >= 18) {
+    console.log("You are allowed");
+} else {
+    console.log("Sorry You are not allowed");
+}
+if (isFemale == true) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+if (driverStatus == "bob") {
+    console.log("you can drive");
+} else {
+    console.log("can't drive");
+}
