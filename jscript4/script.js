932fce5a8f1494fe59a01afa4ea885215ffaffd0
Ramanan663 <69298210+Ramanan663@users.noreply.github.com>
2020-09-30 14:53:53 +0200

    Add files via upload

--- jscript4/script.js
+++ jscript4/script.js
@@ -1,29 +1,20 @@
-//Deel 1: 50% korting
-const age = 30;
-
-if (age >= 18 && age <= 25) {
-    console.log("je krijgt 50% korting");
-} else {
-    console.log("geen korting");
-}
-//Deel 2: Ludieke actie!
-const nam = "Bram";
-if (nam == "Sarah" || nam == "Abraham") {
-    console.log("een gratis biertje! ");
-} else {
-    console.log("sorry moet betalen");
-}
-//Deel 3: Jubileum korting
-console.log(
-    "bestelling boven 25 euro -  krijg je gratis (vega)bitterballen,bestelling boven 50 euro-krijg je gratis portie nachos,bestelling boven 50 euro- krijg je een gratis flesje champagne"
-);
-const totalAmount = 115;
-if (totalAmount >= 25 && totalAmount <= 50) {
-    console.log(" krijg je gratis (vega)bitterballen");
-} else if (totalAmount >= 50 && totalAmount < 100) {
-    console.log("krijg je gratis portie nachos");
-} else if (totalAmount >= 100) {
-    console.log(" krijg je een gratis flesje champagne");
-} else {
-    console.log("sorry");
-}
+var person = { name: "ramanan", age: 39 };
+//console.log(person);
+// console.log(name);
+//consloe.log(age);
+//console.log(person.name);
+//console.log(person.age);
+//console.log(person["name"]);
+//console.log(person["age"]);
+//var evaluations = [7, 10, 9];
+//console.log(evaluations);
+var color = ["groen", "blauw", "rood"];
+//console.log(color);
+//console.log(color.length);
+//console.log(color[0]);
+//console.log(color[2]);
+//var total = color.push("geel", "5");
+//console.log(color);
+var total = color.push({ greeting: "Hi ik ben een object" });
+//console.log(color);
+console.log(color[3]);
