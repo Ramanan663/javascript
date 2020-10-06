5eddbb330ea0829ae17a342948834644544cfb6f
Ramanan663 <69298210+Ramanan663@users.noreply.github.com>
2020-10-06 16:31:40 +0200

    Add files via upload

---
+++ opdracht14.2/multiplication-tables.js
@@ -0,0 +1,8 @@
+/*for (let i = 1; i <= 10; i++) {
+    console.log(i + " x 9 = " + i * 9);
+}*/
+const multipel = 9;
+for (let i = 0; i <= 10; i++) {
+    let result = multipel * i;
+    console.log(multipel + " * " + i + " = " + result);
+}
