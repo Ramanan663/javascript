a85dd86f91166e1ae126fc83c16c8f9f65db59b8
Ramanan663 <69298210+Ramanan663@users.noreply.github.com>
2020-10-07 13:28:24 +0200

    Add files via upload

---
+++ opdracht14.3/script.js
@@ -0,0 +1,17 @@
+/*const colorArray = ["yellow", "blue", "red", "orange"];
+let i = 0;
+while (i < colorArray.length) {
+    console.log(colorArray[i]);
+    i++;
+}*/
+const colorArray = ["yellow", "blue", "red", "orange"];
+
+/*for (let i = 0; i < colorArray.length; i++) {
+    console.log(colorArray[i]);
+}*/
+/*colorArray.forEach(function(element) {
+    console.log(element + "  ");
+});*/
+colorArray.forEach((Element) => {
+    console.log(Element);
+});
